1c6bd2fc972b3c2db1f4724e16d942ec5592dc0a
shestermonki <faustinoludwin@gmail.com>
2024-04-18 19:53:53 +0200

    style_v6 --main index

--- script.js
+++ script.js
@@ -1,11 +1,55 @@
 fetch('../header/header.html')
-    .then(response => response.text())
-    .then(html => {
-        document.getElementById('contenedor').innerHTML = html;
-    });
+  .then(response => response.text())
+  .then(html => {
+    document.getElementById('contenedor').innerHTML = html;
+  });
 
-    fetch('../footer/footer.html')
-    .then(response => response.text())
-    .then(html => {
-      document.getElementById('contenedor02').innerHTML = html;
-    }); 
+fetch('../footer/footer.html')
+  .then(response => response.text())
+  .then(html => {
+    document.getElementById('contenedor02').innerHTML = html;
+  });
+
+//HOME
+
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute(
+    "class",
+    isOpen ? "ri-close-line" : "ri-menu-3-line"
+  );
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-3-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// container
+ScrollReveal().reveal(".container__content h1", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".container__content h2", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".container__content p", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".socials", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
